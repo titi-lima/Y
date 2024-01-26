919c6d3bef638b9bad3394b6a3da25fa2a1189ed
titi-lima <titisau@gmail.com>
2024-01-26 18:47:56 -0300

    feat: integration example

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import axios from "axios";
+import Image from "next/image";
+async function getData() {
+  const res = await axios.post("http://localhost:3001/users", {
+    name: "John",
+    nickName: "John",
 
+  });
+}
 export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  )
+  return <main></main>;
 }
