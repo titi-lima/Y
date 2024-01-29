2b0bbc6e1143724e4a658caa11febf80775196fb
damattag <guilherme.damatta@yahoo.com.br>
2024-01-29 10:56:05 -0300

    created auth routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import userRouter from './UserRoutes';
+import authRouter from './AuthRoutes';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/sessions', authRouter);
 
 router.route('/').get((_, res) => {
   res.status(200).send('ok');
