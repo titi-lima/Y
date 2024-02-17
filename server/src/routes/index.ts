34ca0eedc70e09072b3b62d8ef471c447256d16b
titi-lima <titisau@gmail.com>
2024-02-17 13:21:52 -0300

    feat(file-upload): global file route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import userRouter from './UserRoutes';
 import authRouter from './AuthRoutes';
+import fileRouter from './FileRoutes';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/sessions', authRouter);
+router.use('/files', fileRouter);
 
 router.route('/').get((_, res) => {
   res.status(200).send('ok');
