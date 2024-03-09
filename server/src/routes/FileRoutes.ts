5dca7a1418eed72f45cb80ee03a3ec89d183bec6
damattag <guilherme.damatta@yahoo.com.br>
2024-03-09 04:13:44 -0300

    next auth basic

--- server/src/routes/FileRoutes.ts
+++ server/src/routes/FileRoutes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import auth from '../middlewares/auth';
 import parser from '../config/multer';
 import { FileController } from '../controllers';
 
@@ -7,6 +6,6 @@ const fileRouter = Router();
 
 fileRouter
   .route('/upload')
-  .post(parser.single('file'), [auth], FileController.uploadFile);
+  .post(parser.single('file'), FileController.uploadFile);
 
 export default fileRouter;
